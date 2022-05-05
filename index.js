dc2f076c774df7fb460692529164fe01c5df80f9
afzal9632 <mdafzal9632@gmail.com>
2022-05-06 01:24:26 +0530

    assignment

---
+++ index.js
@@ -0,0 +1,59 @@
+document.querySelector("#form").addEventListener("submit",myfun);
+
+function myfun(){
+    event.preventDefault();
+var name=document.querySelector("#name").value;
+console.log(name)
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=00b3ec0088842608a7220109e95aa9fd`;
+//const url=`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=5881c4a70f1f474bc5289105d70aa1b5`;
+fetch(url)
+.then(function (res){
+    return res.json();
+})
+.then(function (res){
+    console.log(res);
+    append(res);
+})
+.catch(function (err){
+    console.log(err);
+});
+
+function append(data){
+    let container = document.getElementById("cont");
+  let map = document.getElementById("gmap_canvas");
+  container.innerHTML = null;
+  map.src = `https://maps.google.com/maps?q=${data.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+
+  let city = document.createElement("p");
+  city.innerText = `City: ${data.name}`;
+  city.style.color="red";
+
+  let min = document.createElement("p");
+  min.innerText = `Min temp:${Math.floor(data.main.temp_min-273)}℃`;
+min.style.color="indigo"
+  let max = document.createElement("p");
+  max.innerText = `Max temp: ${Math.floor(data.main.temp_max-273)}℃`;
+  max.style.color="green"
+  let wind=document.createElement("p");
+    wind.innerText=`Wind speed: ${data.wind.speed}m/s` 
+    wind.style.color="green" 
+  let clouds=document.createElement("p");
+  clouds.innerText=`Cloud: ${data.clouds.all}%`;
+  clouds.style.color="blue"
+  let sunrise=document.createElement("p");
+  sunrise.innerText=`Sunrise: ${data.sys.sunrise}`;
+  sunrise.style.color="red"
+  let sunset=document.createElement("p");
+  sunset.innerText=`Sunset: ${data.sys.sunset}`;
+  sunset.style.color="violet"
+
+  let current = document.createElement("p");
+  current.innerText = `Current Temp: ${Math.floor(data.main.temp-273)}℃`;
+  current.style.color="brown"
+  let humidity = document.createElement("p");
+  humidity.innerText = `Humidity: ${data.main.humidity}%`;
+  humidity.style.color="teal"
+ 
+  container.append(city, min,max,wind,sunrise,sunset,clouds,current, humidity);
+}
+}
